f95822c5a5ebe80cd79dfdf9980f8935954ebaf5
Alexander R Torrijos <artorrijos@gmail.com>
2024-03-29 16:04:29 +0800

    Encryption Decryption of cookie working

--- Authentication/Program.cs
+++ Authentication/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDataProtection();
 
 var app = builder.Build();
 
-app.MapGet("/username", (HttpContext ctx) =>
+app.MapGet("/username", (HttpContext ctx, IDataProtectionProvider idp) =>
 {
+    var protector = idp.CreateProtector("auth-cookie");
+
     var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(c => c.StartsWith("auth="));
     if (authCookie == null)
     {
@@ -11,14 +17,16 @@ app.MapGet("/username", (HttpContext ctx) =>
         return Results.Unauthorized();
     }
 
-    var value = authCookie.Split('=').Last().Split(':').Last();
+    var protectedPayload = authCookie.Split('=').Last();
+    var value = protector.Unprotect(protectedPayload).Split(':').Last();
 
     return Results.Ok(value);
 });
 
-app.MapGet("/login", (HttpContext ctx) =>
+app.MapGet("/login", (HttpContext ctx, IDataProtectionProvider idp) =>
 {
-    ctx.Response.Headers.TryAdd("set-cookie", "auth=usr:jane");
+    var protector = idp.CreateProtector("auth-cookie");
+    ctx.Response.Headers.TryAdd("set-cookie", $"auth={protector.Protect("usr:jane")}");
     return Results.Ok("Logged in");
 });
 
